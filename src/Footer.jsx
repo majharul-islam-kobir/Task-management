a5f5d5e98b62d20e4dc00c71e4343ed530306c67
majharul-islam-kobir <majhar13579@gmail.com>
2024-10-27 10:28:14 +0600

    update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer() {
   return (
     <div>
-        <p className="text-center my-6 dark:text-stone-300">&copy; Majharul Islam Kobr All Rihgt Reserved</p>
+        <p className="text-center my-6 dark:text-stone-300">&copy; Majharul Islam Kobir All Rihgt Reserved</p>
     </div>
   )
 }
--- src/section/Banner.jsx
+++ src/section/Banner.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import Container from "../components/Container"
-import banner from "../images/task 1.png"
+import banner from "../images/tampleit.png"
 
 function Banner() {
   return (
@@ -9,8 +9,8 @@ function Banner() {
             <h2 className="text-5xl font-bold text-red-800 dark:text-yellow-300 mb-4">Task  Management</h2>
             <p className="text-[18px] dark:text-yellow-50 md:w-[100%] w-full">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum</p>
         </div>
-        <div className="mx-auto">
-            <img className="max-w-sm hidden md:block" src={banner} alt="banner-image" />
+        <div className="ms-auto">
+            <img className=" w-[300px] rounded-md mt-4 max-w-sm hidden md:block" src={banner} alt="banner-image" />
         </div>
 
     </Container>
